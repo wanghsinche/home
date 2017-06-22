66510cf64aab5001015d775b3677858caabb65aa
王鑫哲 <hzwangxinzhe1@corp.netease.com>
2017-06-22 15:46:14 +0800

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,64 @@
 __inline('./module/loader.js');
-__inline('./module/cube.js');
+__inline('./module/vr.js');
 
 // 所有模块都通过 define 来定义
 (function(require, $) {
-	'use strict';
+  'use strict';
   // 通过 require 引入依赖
   var loader = require('loader');
-  var cube = require('cube');
+  var vr = require('vr');
   loader.init({
-  	CB:function(){
-  		console.log($);
-  	}
+    CB: function() {
+      console.log($);
+    }
   });
+  vr.loadThreeJS(function() {
+    vr.loadClickEles([{
+      //首饰
+      className: 'vrclickele',
+      id: 'vrele0',
+      position: [-450, -110, 20],
+      rotation: [0, Math.PI / 2, 0], //vr.lookAtOri([-450, -110, 10]),
+      cb: function() {
+        alert('css渲染元素');
+      }
+    }, {
+      //鼓
+      className: 'vrclickele',
+      id: 'vrele1',
+      position: [-80, -100, 400],
+      rotation: [0, 0, 0], //vr.lookAtOri([-120, -100, 400]),
+      cb: function() {
+        alert('css渲染元素');
+      }
+    }, {
+      //桌
+      className: 'vrclickele',
+      id: 'vrele2',
+      position: [0, -100, -400],
+      rotation: [0, 0, 0], //vr.lookAtOri([-350, -0, 400]),//[0,Math.PI/6,0],
+      cb: function() {
+        alert('css渲染元素');
+      }
+    }, {
+      //琴
+      className: 'vrclickele',
+      id: 'vrele3',
+      position: [-120, -200, 450],
+      rotation: [0, -Math.PI / 18, 0], //vr.lookAtOri([-120, -100, 400]),
+      cb: function() {
+        alert('css渲染元素');
+      }
+    }]);
+    try {
+      vr.init('vrbox');
+    } catch (e) {
+      alert('该浏览器不支持threejs，请换个浏览器打开');
+    }
+
+    vr.toggleDevice();
+  });
+
   // 通过 exports 对外提供接口
   // exports.doSomething = function(){
 
--- src/js/module/loader.js
+++ src/js/module/loader.js
@@ -1,40 +1,57 @@
 // 所有模块都通过 define 来定义
-define('loader',function(require, exports, module) {
-	'use strict';
+define('loader', function(require, exports, module) {
+  'use strict';
   // 通过 require 引入依赖
   var resls = [];
   // 通过 exports 对外提供接口
-  exports.init = function(opts){
-  	var loaderEle, app;
-  	opts = opts||{};
-  	opts.loaderEle = opts.loaderEle || 'loader';
-  	opts.app = opts.app || 'app';
-  	loaderEle = document.getElementById(opts.loaderEle);
-  	app = document.getElementById(opts.app);
-  	loaderEle.style.display = loaderEle.style.display === 'none'?'block':loaderEle.style.display;
-  	app.style.display = 'none';
-  	setTimeout(function(){
+  exports.init = function(opts) {
+    var loaderEle, app;
+    opts = opts || {};
+    opts.loaderEle = opts.loaderEle || 'loader';
+    opts.app = opts.app || 'app';
+    loaderEle = document.getElementById(opts.loaderEle);
+    app = document.getElementById(opts.app);
+    loaderEle.style.display = loaderEle.style.display === 'none' ? 'block' : loaderEle.style.display;
+    app.style.display = 'none';
+
+
+    var counter = 0,
+      tmpscr = null;
+    if (resls.length > 0) {
+      resls.forEach(function(link) {
+        tmpscr = document.createElement('img');
+        tmpscr.onload = function() {
+          counter++;
+          if (counter === resls.length) {
+            allloaded();
+          }
+        };
+        tmpscr.src = link;
+      });
+    } else {
+      allloaded();
+    }
+
+    function allloaded() {
       loaderEle.className += 'animated zoomOut';
-      loaderEle.addEventListener('webkitAnimationEnd', function cb(){
+      loaderEle.addEventListener('webkitAnimationEnd', function cb() {
         loaderEle.style.display = 'none';
         app.style.display = 'block';
-        loaderEle.removeEventListener('webkitAnimationEnd',cb);
-      });  //  Chrome, Safari 和 Opera
-      loaderEle.addEventListener("animationend", function cb(){
+        loaderEle.removeEventListener('webkitAnimationEnd', cb);
+      }); //  Chrome, Safari 和 Opera
+      loaderEle.addEventListener('animationend', function cb() {
         loaderEle.style.display = 'none';
         app.style.display = 'block';
-        loaderEle.removeEventListener('animationend',cb);
-      });        // 标准语法
-	  	
-	  	if(typeof opts.CB === 'function'){
-	  		opts.CB();
-	  	}
-  	},3000);
+        loaderEle.removeEventListener('animationend', cb);
+      }); // 标准语法
 
+      if (typeof opts.CB === 'function') {
+        opts.CB();
+      }
+
+    }
+    exports.load = function(ls) {
+      resls = ls;
+    };
   };
-  exports.load = function(ls){
-    resls = ls;
-  };
-
-
 });
